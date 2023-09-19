2e56a38e92464c06916cffc1b70d532ee2d2e817
Chunghee Hwang <hch0821@gmail.com>
2023-09-19 12:08:02 +0900

    fix: 임시 폴더 가져오는 로직 변경 docker로 띄운 서버와 로컬과 경로가 다르면 파일을 가져오지 못하는 오류 제거

--- src/main/java/com/chung/lifusic/core/service/FileStorageService.java
+++ src/main/java/com/chung/lifusic/core/service/FileStorageService.java
@@ -3,6 +3,7 @@ package com.chung.lifusic.core.service;
 import com.chung.lifusic.core.common.utils.StringUtil;
 import com.chung.lifusic.core.exception.FileStorageException;
 import com.chung.lifusic.core.dto.FileDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
@@ -14,29 +15,35 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 @Service
+@Slf4j
 public class FileStorageService {
 
     @Value("${file.upload.directory}")
-    private String tempUploadDirectory;
+    private String uploadDirectory;
+
+    @Value("${file.upload.temp-directory-name}")
+    private String tempDirectoryName;
 
     /**
      * 파일을 temp directory에 저장
      */
     public FileDto storeFileToTempDir(MultipartFile file, String fileName) {
         try {
-            Files.createDirectories(getTempFileDirectoryPath());
+            Files.createDirectories(getTempDirectoryPath());
             final String fileExtension = StringUtils.getFilenameExtension(file.getOriginalFilename());
             final String fileNameWithExtension = fileName + "." + fileExtension;
-            Path targetLocation = this.getTempFileDirectoryPath().resolve(fileNameWithExtension);
+            Path targetLocation = this.getTempDirectoryPath().resolve(fileNameWithExtension);
+            final String tempFilePath = this.tempDirectoryName + "/" + fileNameWithExtension;
             Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
             return FileDto
                     .builder()
-                    .tempFilePath(targetLocation.toAbsolutePath().toString())
+                    .tempFilePath(tempFilePath)
                     .originalFileName(file.getOriginalFilename())
                     .contentType(file.getContentType())
                     .size(file.getSize())
                     .build();
         } catch (Exception exception) {
+            log.error("Fail to save file to temp directory: {}", exception.getMessage());
             throw new FileStorageException("Could not store file.");
         }
     }
@@ -46,7 +53,11 @@ public class FileStorageService {
         return storeFileToTempDir(file, randomName);
     }
 
-    private Path getTempFileDirectoryPath() {
-        return Paths.get(tempUploadDirectory).toAbsolutePath().normalize();
+    private Path getUploadDirectoryPath() {
+        return Paths.get(uploadDirectory).toAbsolutePath();
+    }
+
+    private Path getTempDirectoryPath() {
+        return getUploadDirectoryPath().resolve(tempDirectoryName);
     }
 }
